f972056be1b98dfa0824289ac639b4867f8464ec
Tanner Prestegard <tprestegard@univa.com>
2020-04-16 12:48:46 -0500

    Reduce bootstrap script file size
    Need to get under 16384 byte limit for AWS. Note that in practice,
    the size will be larger than shown in the repo because these are just
    templates; we add settings and the insertnode_request during
    processing.

--- tortuga_kits/gceadapter/files/bootstrap-gcp-ubuntu18.py
+++ tortuga_kits/gceadapter/files/bootstrap-gcp-ubuntu18.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python3
 
-import itertools
-import json
-import os
-import random
-import socket
-import subprocess
-import sys
-import tempfile
-import time
+import itertools, json, os, random, socket, subprocess, sys, tempfile, time
 
 # Default settings
 operating_system = 'debian'
@@ -34,25 +26,19 @@ class ReqMixin:
     class InvalidCredentials(Exception):
         pass
 
-    def req(self, u, h=None, d=None):
-        _req = getattr(
-            self, '_req_py{}'.format(self._get_py_maj()))
-        return _req(u, h, d)
+    def req(self, u, h={}, d=None):
+        return getattr(self, '_req_py{}'.format(self._get_py_maj()))(u, h, d)
 
     def _get_py_maj(self):
         return sys.version_info[0]
 
-    def _req_py2(self, u, h=None, d=None):
+    def _req_py2(self, u, h={}, d=None):
         import urllib2
         req = urllib2.Request(u)
-        if h:
-            for k, v in h.items():
-                req.add_header(k, v)
+        for k, v in h.items():
+            req.add_header(k, v)
         try:
-            if d:
-                res = urllib2.urlopen(req, d)
-            else:
-                res = urllib2.urlopen(req)
+            res = urllib2.urlopen(req, data=d if d else None)
         except urllib2.HTTPError as ex:
             if ex.code == 404:
                 raise self.NotFound(str(ex))
@@ -63,19 +49,14 @@ class ReqMixin:
             raise Exception('Unable to read URL: {}'.format(u))
         return res.read()
 
-    def _req_py3(self, u, h=None, d=None):
+    def _req_py3(self, u, h={}, d=None):
         from urllib import request, error
         req = request.Request(u)
-        if isinstance(d, str):
-            d = d.encode()
-        if h:
-            for k, v in h.items():
-                req.add_header(k, v)
+        d = d.encode() if isinstance(d, str) else d
+        for k, v in h.items():
+            req.add_header(k, v)
         try:
-            if d:
-                res = request.urlopen(req, d)
-            else:
-                res = request.urlopen(req)
+            res = request.urlopen(req, data=d if d else None)
         except error.HTTPError as ex:
             if ex.code == 404:
                 raise self.NotFound(str(ex))
@@ -102,21 +83,24 @@ class ProviderBase(ReqMixin):
 
 
 class BootstrapperBase(ReqMixin):
-    def __init__(self, cloud_provider_helper, installer_hostname,
-                 installer_ip_address, port, override_dns_domain=None,
-                 dns_domain=None, dns_search=None, dns_options=None,
-                 dns_nameservers=None, insertnode_request=None):
-        self.cloud_provider_helper = cloud_provider_helper
-        self.cloud_provider_helper.set_bootstrapper(self)
-        self.installer_hostname = installer_hostname
-        self.installer_ip_address = installer_ip_address
+    cert_loc = "/etc/pki/ca-trust/source/anchors"
+    cert_ext = "pem"
+    cert_update_cmd = "update-ca-trust"
+
+    def __init__(self, provider, inst_hname, inst_ip, port,
+                 override_dns_domain=None, dns_domain=None, dns_search=None,
+                 dns_opts=None, dns_nameservers=None, insertnode_req=None):
+        self.provider = provider
+        self.provider.set_bootstrapper(self)
+        self.inst_hname = inst_hname
+        self.inst_ip = inst_ip
         self.port = port
         self.override_dns_domain = override_dns_domain
         self.dns_domain = dns_domain
         self.dns_search = dns_search
-        self.dns_options = dns_options
+        self.dns_opts = dns_opts
         self.dns_nameservers = dns_nameservers
-        self.insertnode_request = insertnode_request
+        self.insertnode_req = insertnode_req
 
     def run(self):
         self.cfg_dns()
@@ -126,20 +110,20 @@ class BootstrapperBase(ReqMixin):
         self.boot_puppet()
 
     def cfg_dns(self):
-        if not self.installer_ip_address:
+        if not self.inst_ip:
             raise Exception('Installer hostname not set')
-        if not self.installer_ip_address:
+        if not self.inst_ip:
             raise Exception('Installer IP address not set')
         with open('/etc/hosts', 'a+') as fp:
             fp.write('{}\t{}\n'.format(
-                self.installer_ip_address, self.installer_hostname))
+                self.inst_ip, self.inst_hname))
         if self.override_dns_domain:
             with open('/etc/resolv.conf', 'w') as fp:
                 fp.write('# Created by Tortuga\n')
                 if self.dns_search:
                     fp.write('search {}}\n'.format(self.dns_search))
-                if self.dns_options:
-                    fp.write('options {}\n'.format(self.dns_options))
+                if self.dns_opts:
+                    fp.write('options {}\n'.format(self.dns_opts))
                 if not self.dns_nameservers:
                     raise Exception("DNS nameservers not set")
                 if not isinstance(self.dns_nameservers, list):
@@ -167,25 +151,23 @@ class BootstrapperBase(ReqMixin):
                 fp.write("\n".join(data))
 
     def add_node(self):
-        if not self.insertnode_request:
+        if not self.insertnode_req:
             print("No insertnode_request, skipping add_node")
             return
-        if not self.installer_ip_address:
+        if not self.inst_ip:
             raise Exception("Installer IP address not set")
-        self.add_installer_ca_certificate()
+        self.add_installer_cert()
         d = {
             'node_details': {
-                'name': self.cloud_provider_helper.get_node_name(),
-                'metadata': self.cloud_provider_helper.get_node_metadata(),
+                'name': self.provider.get_node_name(),
+                'metadata': self.provider.get_node_metadata(),
             }
         }
         print('Instance details: ' + json.dumps(d))
         u = 'https://{}:{}/v1/node-token/{}'.format(
-            self.installer_hostname, self.port,
-            self.insertnode_request.decode())
-        h = {
-            'Content-Type': 'application/json'
-        }
+            self.inst_hname, self.port,
+            self.insertnode_req.decode())
+        h = {'Content-Type': 'application/json'}
         for i in range(5):
             try:
                 print('Add node: {}'.format(u))
@@ -202,14 +184,12 @@ class BootstrapperBase(ReqMixin):
             raise Exception('Unable to communicate with Tortuga')
         print(json.loads(res))
 
-    def add_installer_ca_certificate(self):
-        self.try_cmd("mkdir -p /etc/pki/ca-trust/source/anchors/")
-        self.try_cmd(
-            "curl http://{}:8008/ca.pem > "
-            "/etc/pki/ca-trust/source/anchors/tortuga-ca.pem".format(
-                self.installer_ip_address)
+    def add_installer_cert(self):
+        self.try_cmd("mkdir -p {}".format(self.cert_loc))
+        self.try_cmd("curl http://{}:8008/ca.pem > {}/tortuga-ca.{}".format(
+            self.inst_ip, self.cert_loc, self.cert_ext)
         )
-        self.try_cmd("update-ca-trust")
+        self.try_cmd(self.cert_update_cmd)
 
     def disable_selinux(self):
         self.try_cmd('setenforce permissive')
@@ -220,25 +200,23 @@ class BootstrapperBase(ReqMixin):
     def boot_puppet(self):
         self.try_cmd('touch /tmp/puppet_bootstrap.log')
         cmd = (
-            '/opt/puppetlabs/bin/puppet agent'
-            ' --logdest /tmp/puppet_bootstrap.log'
-            ' --no-daemonize'
-            ' --onetime --server {}'
-            ' --waitforcert 120'.format(self.installer_hostname)
+            '/opt/puppetlabs/bin/puppet agent --logdest '
+            '/tmp/puppet_bootstrap.log --no-daemonize --onetime --server {}'
+            ' --waitforcert 120'.format(self.inst_hname)
         )
         self.try_cmd(cmd, good_return_values=(0, 2), time_limit=10 * 60)
 
-    def try_cmd(self, command, good_return_values=(0,), retry_limit=0,
+    def try_cmd(self, cmd, good_return_values=(0,), retry_limit=0,
                 time_limit=0, max_sleep_time=15000, sleep_interval=2000):
-        total_sleep_time = 0
+        total_sleep = 0
         for retries in itertools.count(0):
-            returned = subprocess.Popen(command, shell=True).wait()
-            if returned in good_return_values or \
-                    retries >= retry_limit or total_sleep_time >= time_limit:
-                return returned
+            rv = subprocess.Popen(cmd, shell=True).wait()
+            if rv in good_return_values or retries >= retry_limit or \
+                    total_sleep >= time_limit:
+                return rv
             seed = min(max_sleep_time, sleep_interval * 2 ** retries)
             sleep_for = (seed / 2 + random.randint(0, seed / 2)) / 1000.0
-            total_sleep_time += sleep_for
+            total_sleep += sleep_for
             time.sleep(sleep_for)
 
 
@@ -273,13 +251,12 @@ class AzureProvider(ProviderBase):
             return socket.getfqdn() + "." + self.bootstrapper.dns_domain
         else:
             return socket.gethostname() + "." + \
-                   self.bootstrapper.installer_hostname.split('.', 1)[1]
+                   self.bootstrapper.inst_hname.split('.', 1)[1]
 
     def get_node_metadata(self):
         instance_name = self._get_inst_data('/compute/name')
         try:
-            scale_set_name = self._get_inst_data(
-                '/compute/vmScaleSetName')
+            scale_set_name = self._get_inst_data('/compute/vmScaleSetName')
             instance_id = int(instance_name.rsplit('_', 1)[1])
         except:
             instance_id = instance_name
@@ -294,9 +271,7 @@ class AzureProvider(ProviderBase):
     def _get_inst_data(self, path):
         u = ('http://169.254.169.254/metadata/instance{}'
                '?api-version=2019-04-30&format=text'.format(path))
-        h = {
-            'Metadata': 'true'
-        }
+        h = {'Metadata': 'true'}
         for i in range(5):
             try:
                 print('Get instance data: {}'.format(u))
@@ -315,15 +290,11 @@ class GcpProvider(ProviderBase):
         return self._get_inst_data('/hostname')
 
     def get_node_metadata(self):
-        return {
-            'instance_name': self._get_inst_data('/name')
-        }
+        return {'instance_name': self._get_inst_data('/name')}
 
     def _get_inst_data(self, path):
         u = 'http://169.254.169.254/computeMetadata/v1/instance' + path
-        h = {
-            'Metadata-Flavor': 'Google'
-        }
+        h = {'Metadata-Flavor': 'Google'}
         for i in range(5):
             try:
                 print('Get instance data: {}'.format(u))
@@ -346,23 +317,22 @@ class CentOsBootstrapper(BootstrapperBase):
         pkg = 'puppet5-release'
         if not self._is_installed(pkg):
             ver = self._get_major_ver()
-            u = ('http://yum.puppetlabs.com/puppet5/'
-                   '{}-el-{}.noarch.rpm'.format(pkg, ver))
-            result = self.try_cmd('rpm -ivh {}'.format(u),
-                                      retry_limit=5)
+            u = 'http://yum.puppetlabs.com/puppet5/{}-el-{}.noarch.rpm'.format(
+                pkg, ver)
+            result = self.try_cmd('rpm -ivh {}'.format(u), retry_limit=5)
             if result != 0:
                 raise Exception('Unable to install package: {}'.format(pkg))
         self._install('puppet-agent')
 
     def _get_major_ver(self):
         result = self.try_cmd(
-            'rpm --query --queryformat %{VENDOR} system-release |'
-            ' grep --quiet --ignore-case Amazon'
+            'rpm --query --queryformat %{VENDOR} system-release | '
+            'grep --quiet --ignore-case Amazon'
         )
         if result == 0:
             # amazon linuxv2
             self.try_cmd(
-                'awk -F: \'{ print $6 }\' /etc/system-release-cpe |'
+                'awk -F: \'{ print $6 }\' /etc/system-release-cpe | '
                 'grep --quiet \'^2$\''
             )
             if result == 0:
@@ -386,6 +356,10 @@ class CentOsBootstrapper(BootstrapperBase):
 
 
 class DebianBootstrapper(BootstrapperBase):
+    cert_loc = "/usr/local/share/ca-certificates"
+    cert_ext = "crt"
+    cert_update_cmd = "update-ca-certificates"
+
     def install_puppet(self):
         if self._is_installed('puppet-agent'):
             return
@@ -448,42 +422,22 @@ class DebianBootstrapper(BootstrapperBase):
         return self.try_cmd(
             'dpkg -l {} 2>/dev/null | grep -q ^ii'.format(pkg)) == 0
 
-    def add_installer_ca_certificate(self):
-        self.try_cmd("mkdir -p /usr/local/share/ca-certificates")
-        self.try_cmd(
-            "curl http://{}:8008/ca.pem > "
-            "/usr/local/share/ca-certificates/tortuga-ca.crt".format(
-                self.installer_ip_address)
-        )
-        self.try_cmd("update-ca-certificates")
 
-
-BOOTSTRAPPERS = {
-    'centos': CentOsBootstrapper,
-    'debian': DebianBootstrapper
-}
-
-
-PROVIDERS = {
-    'aws': AwsProvider,
-    'azure': AzureProvider,
-    'gcp': GcpProvider,
-}
+BOOTSTRAPPERS = {'centos': CentOsBootstrapper, 'debian': DebianBootstrapper}
+PROVIDERS = {'aws': AwsProvider, 'azure': AzureProvider, 'gcp': GcpProvider}
 
 
 if __name__ == '__main__':
-    Bootstrapper = BOOTSTRAPPERS[operating_system]
-    Provider = PROVIDERS[cloud_provider]
-    bs = Bootstrapper(
-        Provider(),
+    bs = BOOTSTRAPPERS[operating_system](
+        PROVIDERS[cloud_provider](),
         installerHostName,
         installerIpAddress,
         port,
         override_dns_domain=override_dns_domain,
         dns_domain=dns_domain,
         dns_search=dns_search,
-        dns_options=dns_options,
+        dns_opts=dns_options,
         dns_nameservers=dns_nameservers,
-        insertnode_request=insertnode_request
+        insertnode_req=insertnode_request
     )
     bs.run()
